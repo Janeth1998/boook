cbd5c6a2f381e6409ac31f6e028d7b1159dda5b4
Janeth Carhuavilca Castillo <castillo.jane2015@gmail.com>
2016-03-10 10:03:38 -0500

    iniciando mundo

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<<meta charset="utf-8">
+	<<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+	<link >></title>
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div class="contenedor">
+<nav>
+	<ul class= "menu-lista">
+			<li class="menu-item">
+			<a href="" class="menu-link">INICIO</a>
+			</li>
+			<li class="menu-item">
+			<a href=""class="menu-link">GALERIA</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">BLOG</a>
+			</li>
+	<li class="menu-item">
+	<a href="" class="menu-link">ANUNCIOS</a>
+	</li>
+	<li class="menu-item">
+	<a href="" class="menu-link">CONTACTENOS</a>
+	
+	</li>
+	</nav>	
+	<div class="slider">
+	<img src="1.jpg" alt="1000"  width="900">
+	<div class="slider">
+	<h1>mis imagenes</h1>
+	<div class="slider">
+	<img src="2.jpg" height="140" nidth"140">
+	<img src="3.jpg" height="140" nidth"140">
+	<img src="4.jpg" height="140" nidth"140">
+	<img src="5.jpg" height="140" nidth"140">
+</body>
+</html>
